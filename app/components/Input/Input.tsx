19de6ff4298eb1a26be430a882c9060c0a3d356f
Alexis Buelvas Madera <alexjesus-4856@hotmail.com>
2024-09-15 02:39:20 -0500

    Add Input component

---
+++ app/components/Input/Input.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { StyledInputProps } from "@/app/types/types.env";
+import TextField from "@mui/material/TextField";
+import { styled } from "@mui/material/styles";
+
+const InputField = styled(TextField)({
+  width: "90%",
+  margin: "1rem 0",
+  "& label.Mui-focused": {
+    color: "#A0AAB4",
+  },
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "#B2BAC2",
+  },
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "#E0E3E7",
+    },
+    "&:hover fieldset": {
+      borderColor: "#B2BAC2",
+    },
+    "&.Mui-focused fieldset": {
+      borderColor: "#6F7E8C",
+    },
+  },
+});
+
+export default function Input({
+  helperText,
+  type,
+  id,
+  label,
+  required,
+}: StyledInputProps) {
+  return (
+    <InputField
+      required={required}
+      helperText={helperText}
+      type={type}
+      id={id}
+      label={label}
+    />
+  );
+}

feef8de10211b2312a62e413ce5319c131df6fc4
Alexis Buelvas Madera <alexjesus-4856@hotmail.com>
2024-09-15 02:39:20 -0500

    Add utility for data form

---
+++ app/utils/dataForm.tsx
@@ -0,0 +1,38 @@
+export const inputFields = [
+  {
+    required: true,
+    helperText: "Ingresa tu nombre de usuario",
+    type: "text",
+    id: "username",
+    label: "Nombre de Usuario",
+  },
+  {
+    required: true,
+    helperText: "",
+    type: "email",
+    id: "email",
+    label: "Correo Electrónico",
+  },
+  {
+    required: true,
+    helperText: "",
+    type: "email",
+    id: "email",
+    label: "Confirma tu correo",
+  },
+  {
+    required: true,
+    helperText:
+      "Tu contraseña debe tener entre 9 y 16 caracteres e incluir números y letras",
+    type: "password",
+    id: "password",
+    label: "Ingrese su contraseña",
+  },
+  {
+    required: true,
+    helperText: "",
+    type: "password",
+    id: "password",
+    label: "Confirmar constraseña",
+  },
+];
